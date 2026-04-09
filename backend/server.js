f505483a8b4c54ed2ad10196ba1c95af35faa50b
harsh3156 <harshkaklotar09@gmail.com>
2026-04-09 15:17:15 +0530

    looking new standard based environment and upgade backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+connectDB();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Car World backend running" });
+});
+
+
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/temp")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -91,6 +102,8 @@ app.post("/login", async (req, res) => {
 
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
