81b11142566b6a5b1a51626436ed490c963bb74a
harsh3156 <harshkaklotar09@gmail.com>
2026-05-15 19:39:50 +0530

    fix:connection strong

--- admin/admin-panel/src/api/axios.js
+++ admin/admin-panel/src/api/axios.js
@@ -8,7 +8,7 @@ const API = axios.create({
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('adminToken');
   if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  return config; git
 });
 
 API.interceptors.response.use(
